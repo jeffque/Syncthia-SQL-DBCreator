b4d52374d6a18193b158c851633327d79640682c
Jefferson Quesado <jeff.quesado@gmail.com>
2015-02-13 15:32:43 -0300

    Tests in test
    Tests now in the junit stuff.
    Also, ignored stuff in target/

--- src/main/java/br/com/jq/syncthia/bdcreator/App.java
+++ src/main/java/br/com/jq/syncthia/bdcreator/App.java
@@ -1,13 +1,10 @@
 package br.com.jq.syncthia.bdcreator;
 
-import br.com.jq.syncthia.bdcreator.schema.SchemaCreator;
-
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		SchemaCreator.createOrMigrateSchema();
 	}
 }
--- src/test/java/br/com/jq/syncthia/AppTest.java
+++ src/test/java/br/com/jq/syncthia/AppTest.java
@@ -1,5 +1,6 @@
 package br.com.jq.syncthia;
 
+import br.com.jq.syncthia.bdcreator.schema.SchemaCreator;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +8,29 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		SchemaCreator.createOrMigrateSchema();
+		assertTrue(true);
+	}
 }
