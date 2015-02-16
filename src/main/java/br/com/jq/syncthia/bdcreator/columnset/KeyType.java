7b198b32a127e165858a5ecb72058fb2320deb40
Jefferson Quesado <jeff.quesado@gmail.com>
2015-02-16 20:30:02 -0300

    TODO notice added

--- src/main/java/br/com/jq/syncthia/bdcreator/columnset/KeyType.java
+++ src/main/java/br/com/jq/syncthia/bdcreator/columnset/KeyType.java
@@ -1,7 +1,7 @@
 package br.com.jq.syncthia.bdcreator.columnset;
 
 public enum KeyType {
-	PRIMARY_KEY, UNIQUE_KEY;
+	PRIMARY_KEY, UNIQUE_KEY; //TODO FOREING_KEY
 
 	public String keySql() {
 		switch (this) {
