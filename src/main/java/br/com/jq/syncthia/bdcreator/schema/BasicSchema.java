13352bc7b91529285b25cd801d074c8fefa2eb34
Jefferson Quesado <jeff.quesado@gmail.com>
2015-02-16 20:58:29 -0300

    Added ViewVersion table to BasicSchema

--- src/main/java/br/com/jq/syncthia/bdcreator/schema/BasicSchema.java
+++ src/main/java/br/com/jq/syncthia/bdcreator/schema/BasicSchema.java
@@ -1,12 +1,14 @@
 package br.com.jq.syncthia.bdcreator.schema;
 
 import br.com.jq.syncthia.bdcreator.schema.basicSchema.TableVersion;
+import br.com.jq.syncthia.bdcreator.schema.basicSchema.ViewVersion;
 
 public class BasicSchema extends SchemaCreator {
 
 	@Override
 	protected void schemaDefinition() {
 		addTable(new TableVersion());
+		addTable(new ViewVersion());
 	}
 
 	@Override
