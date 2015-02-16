4702d20e9a61584d4696efecbc0e65af47680062
Jefferson Quesado <jeff.quesado@gmail.com>
2015-02-16 20:05:29 -0300

    Basic schema with real table
    Table_version table created

--- src/main/java/br/com/jq/syncthia/bdcreator/schema/BasicSchema.java
+++ src/main/java/br/com/jq/syncthia/bdcreator/schema/BasicSchema.java
@@ -1,50 +1,12 @@
 package br.com.jq.syncthia.bdcreator.schema;
 
-import br.com.jq.syncthia.bdcreator.column.Column;
-import br.com.jq.syncthia.bdcreator.table.Table;
-import br.com.jq.syncthia.bdcreator.table.migration.MigrationSqlCommandStrategy;
+import br.com.jq.syncthia.bdcreator.schema.basicSchema.TableVersion;
 
 public class BasicSchema extends SchemaCreator {
 
 	@Override
 	protected void schemaDefinition() {
-		Table table = new Table();
-		
-		table.setName("CAROLINA");
-		
-		Column nome = new Column();
-		nome.setName("NAME");
-		nome.setSize(50);
-		nome.setType("VARCHAR");
-		Column idade = new Column();
-		idade.setName("AGE");
-		idade.setType("INT");
-		Column address = new Column();
-		address.setName("ADDRESS");
-		address.setType("VARCHAR");
-		address.setSize(100);
-		
-		table.addColumn(nome);
-		table.addColumn(idade);
-		table.addColumn(address);
-		
-		table.setDesiredVersion("v3");
-		table.setSchemaVersion("v1");
-		
-		MigrationSqlCommandStrategy migration1 = new MigrationSqlCommandStrategy();
-		migration1.setNewVersion("v2");
-		migration1.setOldVersion("v1");
-		migration1.setSqlMigration("ALTER TABLE CAROLINA ADD COLUMN NAME VARCHAR(50)");
-		
-		MigrationSqlCommandStrategy migration2 = new MigrationSqlCommandStrategy();
-		migration2.setNewVersion("v3");
-		migration2.setOldVersion("v2");
-		migration2.setSqlMigration("ALTER TABLE CAROLINA ADD COLUMN ADDRESS VARCHAR(100)");
-		
-		table.addMigrationStrategy(migration2);
-		table.addMigrationStrategy(migration1);
-		
-		addTable(table);
+		addTable(new TableVersion());
 	}
 
 	@Override
---
+++ src/main/java/br/com/jq/syncthia/bdcreator/schema/basicSchema/TableVersion.java
@@ -0,0 +1,43 @@
+package br.com.jq.syncthia.bdcreator.schema.basicSchema;
+
+import br.com.jq.syncthia.bdcreator.column.Column;
+import br.com.jq.syncthia.bdcreator.columnset.KeyType;
+import br.com.jq.syncthia.bdcreator.columnset.TableKey;
+import br.com.jq.syncthia.bdcreator.table.Table;
+
+public class TableVersion extends Table {
+	public TableVersion() {
+		setName("TABLE_VERSION");
+		
+		Column pkCol = new Column();
+		Column tableName = new Column();
+		Column tableSchemaVersion = new Column();
+		
+		pkCol.setName("PK");
+		pkCol.setType("INT");
+		pkCol.setNullable(false);
+		
+		tableName.setName("TABLE_NAME");
+		tableName.setType("STRING");
+		tableName.setNullable(false);
+		
+		tableSchemaVersion.setName("TABLE_SCHEMA_VERSION");
+		tableSchemaVersion.setType("STRING");
+		tableSchemaVersion.setNullable(false);
+		
+		addColumn(pkCol);
+		addColumn(tableName);
+		addColumn(tableSchemaVersion);
+		
+		TableKey pk = new TableKey(KeyType.PRIMARY_KEY);
+		pk.setName("PRIMARY_KEY_CONSTRAINT");
+		pk.addColumn(pkCol);
+		
+		TableKey uniqueName = new TableKey(KeyType.UNIQUE_KEY);
+		uniqueName.setName("TABLE_NAME_CONSTRAINT");
+		uniqueName.addColumn(tableName);
+		
+		addKey(pk);
+		addKey(uniqueName);
+	}
+}
