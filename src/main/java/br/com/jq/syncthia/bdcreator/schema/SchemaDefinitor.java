2cad6874c1bf2f6e2cb582a8cf9e08f434ad4528
Jefferson Quesado <jeff.quesado@gmail.com>
2015-03-05 00:36:31 -0300

    SchemaDefinitor

---
+++ src/main/java/br/com/jq/syncthia/bdcreator/schema/SchemaDefinitor.java
@@ -0,0 +1,124 @@
+package br.com.jq.syncthia.bdcreator.schema;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.jq.syncthia.bdcreator.interfaces.Connectable;
+import br.com.jq.syncthia.bdcreator.interfaces.Versionable;
+import br.com.jq.syncthia.bdcreator.table.MigratableSelectable;
+import br.com.jq.syncthia.bdcreator.table.Table;
+import br.com.jq.syncthia.bdcreator.table.View;
+
+abstract class SchemaDefinitor implements Connectable, Versionable {
+	public abstract String getSchemaName();
+	
+	private List<Table> schemaTables;
+	private List<View> schemaViews;
+	private List<MigratableSelectable> schemaMigratables;
+	
+	private String registeredVersion;
+	private String desiredVersion;
+	
+	public SchemaDefinitor() {
+		schemaTables = new ArrayList<Table>();
+		schemaViews = new ArrayList<View>();
+		schemaMigratables = new ArrayList<MigratableSelectable>();
+		
+		registeredVersion = "";
+		desiredVersion = "";
+	}
+	
+	private Connection sqlConnection;
+	
+	@Override
+	public String getDesiredVersion() {
+		return desiredVersion;
+	}
+	
+	@Override
+	public void setDesiredVersion(String desiredVersion) {
+		this.desiredVersion = desiredVersion;
+	}
+	
+	@Override
+	public String getRegisteredVersion() {
+		return registeredVersion;
+	}
+	
+	@Override
+	public void setRegisteredVersion(String registeredVersion) {
+		this.registeredVersion = registeredVersion;
+	}
+	
+	@Override
+	public Connection getConnection() {
+		return sqlConnection;
+	}
+	
+	@Override
+	public void setConnection(Connection sqlConnection) {
+		this.sqlConnection = sqlConnection;
+		
+		for (MigratableSelectable m: getMigratables()) {
+			m.setConnection(sqlConnection);
+		}
+	}
+	
+	public List<Table> getTables() {
+		return schemaTables;
+	}
+	
+	public List<View> getViews() {
+		return schemaViews;
+	}
+	
+	public List<MigratableSelectable> getMigratables() {
+		return schemaMigratables;
+	}
+	
+	public Table getTable(String tableName) {
+		for (Table t: schemaTables) {
+			if (tableName.equals(t.getName())) {
+				return t;
+			}
+		}
+		
+		return null;
+	}
+	
+	public View getView(String viewName) {
+		for (View v: schemaViews) {
+			if (viewName.equals(v.getName())) {
+				return v;
+			}
+		}
+		
+		return null;
+	}
+	
+	public MigratableSelectable getMigratable(String migratableName) {
+		for (MigratableSelectable m: schemaMigratables) {
+			if (migratableName.equals(m.getName())) {
+				return m;
+			}
+		}
+		
+		return null;
+	}
+	
+	protected void addTable(Table t) {
+		addMigratable(t);
+		schemaTables.add(t);
+	}
+
+	protected void addView(View v) {
+		addMigratable(v);
+		schemaViews.add(v);
+	}
+	
+	private void addMigratable(MigratableSelectable m) {
+		schemaMigratables.add(m);
+		m.setConnection(getConnection());
+	}
+}
