8969a9b7cff4e1ba496fa56910cf554beff2646f
Jefferson Quesado <jeff.quesado@gmail.com>
2015-04-07 12:59:16 -0300

    Foreign key now should work

--- src/main/java/br/com/jq/syncthia/bdcreator/columnset/ForeignKey.java
+++ src/main/java/br/com/jq/syncthia/bdcreator/columnset/ForeignKey.java
@@ -1,12 +1,20 @@
 package br.com.jq.syncthia.bdcreator.columnset;
 
+import br.com.jq.syncthia.bdcreator.column.Column;
 import br.com.jq.syncthia.bdcreator.table.Table;
 
+// For now, it only supports referring to the primary key of another table; maybe in the future it may refer to a specific column set
+//TODO on delete/on update actions
+//TODO deferred key support
 public class ForeignKey extends TableColumnSet {
 	private Table tableRefered;
 	private String tableReferedName;
 
 	public Table getTableRefered() {
+		if (tableRefered == null && tableReferedName != null) {
+			// A refered table may be in another schema within the same scehma collection
+			tableRefered = getTable().getSchema().getSchemaCollection().getTable(tableReferedName);
+		}
 		return tableRefered;
 	}
 
@@ -15,6 +23,9 @@ public class ForeignKey extends TableColumnSet {
 	}
 
 	public String getTableReferedName() {
+		if (tableReferedName == null && tableRefered != null) {
+			tableReferedName = tableRefered.getName();
+		}
 		return tableReferedName;
 	}
 
@@ -22,4 +33,27 @@ public class ForeignKey extends TableColumnSet {
 		this.tableReferedName = tableReferedName;
 	}
 
+	public String keyDescription() {
+		StringBuilder builder = new StringBuilder();
+		
+		if (getName() != null) {
+			builder.append(" CONSTRAINT ");
+			builder.append(getName()).append(" ");
+		}
+		
+		builder.append(" FOREIGN KEY (");
+		
+		boolean firstTime = true;
+		for (Column col: columnList) {
+			if (!firstTime) {
+				builder.append(", ");
+			} else {
+				firstTime = false;
+			}
+			builder.append(col.getName());
+		}
+		builder.append(") REFERENCES ").append(getTableReferedName());
+		
+		return builder.toString();
+	}
 }
